165f7debdf86ae0a3380a2234d1731645db81f89
frankzyx <frank91zyx@gmail.com>
2017-07-19 09:00:22 -0400

    Create Person.h

---
+++ Ch7/Person.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using std::string;
+
+struct Person {
+	string name;
+	string address;
+
+	string getName() const { return name; }
+	string getAddress() const { return address; }
+}
